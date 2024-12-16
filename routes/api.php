82253c1315bd52f575e62fc4e0113d14f2eef0a1
NlengDominique <luqnleng5@gmail.com>
2024-12-16 16:33:35 +0100

    ajout des relations

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
+
+Route::get('/tasks', [TaskController::class, 'index']);
+//Route::post('/tasks',[TaskController::class,'store']);
+//Route::get('/tasks/{id}',[TaskController::class,'show']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
